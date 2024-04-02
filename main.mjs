8a7d483d0aa9cb31d18961eeebd04494c9d3f78d
Dwar-liberin <aadarsh@liberintechnologies.com>
2024-04-02 18:34:56 +0530

    add animation

--- main.mjs
+++ main.mjs
@@ -1,113 +1,35 @@
-import { MindARThree } from "mindar-image-three";
-import * as THREE from "three";
-import { videoLoader } from "./libs/videoLoader/videoLoader.js";
-import { loadTexture } from "./libs/loader.js";
-// import { imageLoader } from "./libs/imageLoader/imageLoader.js";
+import { loadGLTF } from "./libs/loader.js";
+const THREE = window.MINDAR.IMAGE.THREE;
 
 document.addEventListener("DOMContentLoaded", () => {
   const start = async () => {
-    const mindarThree = new MindARThree({
+    const mindarThree = new window.MINDAR.IMAGE.MindARThree({
       container: document.body,
-      imageTargetSrc: "./assets/targets/osunioDemo.mind",
+      imageTargetSrc: "./assets/targets/musicband.mind",
     });
     const { renderer, scene, camera } = mindarThree;
 
     const light = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 1);
-
-    const { videoPlane, video } = await videoLoader({
-      path: "./assets/videos/iplreal.mp4",
-    });
-    const isIOS = navigator.appVersion.indexOf("Mac") != -1 ? true : false;
-    video.muted = isIOS;
-
-    // const { planeMesh } = await imageLoader({
-    //   src: "./assets/logo/Button.png",
-    // });
-
-    const iconGeometry = new THREE.PlaneGeometry(1.3, 0.4);
-
-    const textureLoader = new THREE.TextureLoader();
-
-    const iconTexture = textureLoader.load("./assets/logo/iconCal3.png");
-
-    const imageMesh = new THREE.MeshBasicMaterial({
-      map: iconTexture,
-    });
-
-    const planeMesh = new THREE.Mesh(iconGeometry, imageMesh);
-
-    planeMesh.scale.set(0.5, 0.5, 0.5);
-
-    // videoPlane.position.set(0, 0, 0);
-
-    videoPlane.userData.clickable = true; // set the user Data to clickable.
-    // videoPlane.scale.set(2, 2, 2);
-
-    planeMesh.position.set(0, -1, 0);
-    planeMesh.userData.clickable = true;
-
-    // const raccoon = await loadGLTF(
-    //   "./assets/models/musicband-raccoon/scene.gltf"
-    // );
-
-    // raccoon.scene.scale.set(0.1, 0.1, 0.1);
-    // raccoon.scene.position.set(0, 1, 0);
-
     scene.add(light);
 
-    // const anchor = mindarThree.addAnchor(1);
+    const gltf = await loadGLTF("./assets/models/musicband-raccoon/scene.gltf");
+    gltf.scene.scale.set(0.1, 0.1, 0.1);
+    gltf.scene.position.set(0, -0.4, 0);
 
-    const anchorVideo = mindarThree.addAnchor(0);
+    const anchor = mindarThree.addAnchor(0);
+    anchor.group.add(gltf.scene);
 
-    // anchor.group.add(raccoon.scene);
+    const mixer = new THREE.AnimationMixer(gltf.scene);
+    const action = mixer.clipAction(gltf.animations[0]);
+    action.play();
 
-    anchorVideo.group.add(videoPlane);
-
-    anchorVideo.group.add(planeMesh);
-
-    // planeMesh.userData.clickable = true;
-
-    // anchorVideo.group.add(raccoon.scene);
-
-    // const image = mindarThree.addAnchor(0);
-    // image.group.add(planeMesh);
-
-    anchorVideo.onTargetFound = () => {
-      video.play();
-    };
-
-    anchorVideo.onTargetLost = () => {
-      video.pause();
-    };
-
-    document.body.addEventListener("click", (e) => {
-      const mouseX = (e.clientX / window.innerWidth) * 2 - 1;
-      const mouseY = -(e.clientY / window.innerHeight) * 2 + 1;
-
-      const mouse = new THREE.Vector2(mouseX, mouseY);
-      const raycaster = new THREE.Raycaster();
-      raycaster.setFromCamera(mouse, camera);
-      const intersects = raycaster.intersectObjects(scene.children, true);
-
-      if (intersects.length > 0) {
-        let o = intersects[0].object;
-
-        while (o.parent && !o.userData.clickable) {
-          o = o.parent;
-        }
-
-        if (o.userData.clickable && o === planeMesh) {
-          window.location.href = "https://dl.osunio.com/tbCg";
-        }
-      }
-    });
-
-    // video.addEventListener("play", () => {
-    //   video.currentTime = 6;
-    // });
+    const clock = new THREE.Clock();
 
     await mindarThree.start();
     renderer.setAnimationLoop(() => {
+      const delta = clock.getDelta();
+      gltf.scene.rotation.set(0, gltf.scene.rotation.y + delta, 0);
+      mixer.update(delta);
       renderer.render(scene, camera);
     });
   };
